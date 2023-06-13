6bb0b0a8799581ca385ea98a8bfc81d18c7075c6
mannan0819 <mannan1994@hotmail.com>
2023-06-12 22:04:42 -0400

    calendar added

---
+++ src/components/calendar.tsx
@@ -0,0 +1,44 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+const events = [
+    { title: 'Meeting', start: new Date() }
+]
+
+export function DemoApp() {
+
+    return (
+        <div>
+            <h1>MY CALENDAR</h1>
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                }}
+                initialView='dayGridMonth'
+                editable={true}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={true}
+                events={events}
+                eventContent={renderEventContent}
+            />
+        </div>
+    )
+}
+
+// a custom render function
+function renderEventContent(eventInfo: { timeText: string, event: { title: string } }) {
+    return (
+        <>
+            <b>{eventInfo.timeText}</b>
+            : <i>{eventInfo.event.title}</i>
+        </>
+    )
+}
